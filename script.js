7d1b8665d0f910aa60d08d737091ba9e991cd177
Colton Bonner <cbonner26118@gmail.com>
2020-02-02 19:57:16 -0700

    Added ingredient list with JS, adjusted styling, and small adjustment to HTML

--- script.js
+++ script.js
@@ -3,12 +3,19 @@
 
 function displayRandomResults(responseJson) {
     $('#results-list').empty();
+    const ingredients = [];
+    for (let i = 0; i < responseJson.recipes[0].extendedIngredients.length; i++) {
+        ingredients.push(`<li>${responseJson.recipes[0].extendedIngredients[i].original}</li>`);
+    }
     $('#results-list').append(
         `<li><h2>${responseJson.recipes[0].title}</h2></li>
         <a href="${responseJson.recipes[0].sourceUrl}" target="_blank"><img src="${responseJson.recipes[0].image}" 
         alt="Photo of ${responseJson.recipes[0].title}"/><br>
         <span>*Click image for blog post*</span>
         </a>
+        <ul>
+        ${ingredients.join('')}
+        </ul>
         <li><p>${responseJson.recipes[0].instructions}</p></li>`
     )
     $('#results').removeClass('hidden');
@@ -16,11 +23,19 @@ function displayRandomResults(responseJson) {
 
 function displayCuisineResults(responseJson) {
     $('#results-list').empty();
+    const ingredients = [];
+    for (let i = 0; i < responseJson.extendedIngredients.length; i++) {
+            ingredients.push(`<li>${responseJson.extendedIngredients[i].original}</li>`);
+    }
     $('#results-list').append(
         `<li><h2>${responseJson.title}</h2></li>
         <a href="${responseJson.sourceUrl}" target="_blank"><img src="${responseJson.image}" 
         alt="Photo of ${responseJson.title}"/><span>*Click image for blog post*</span>
         </a>
+        <ul>
+        <p>Ingredients:</p>
+        ${ingredients.join('')}
+        </ul>
         <li><p>${responseJson.instructions}</p></li>`
     )
     $('#results').removeClass('hidden');
